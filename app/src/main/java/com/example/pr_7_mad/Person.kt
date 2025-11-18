5ecfcddc5a0b05c0dd65e57958a260e2a28ff7b5
JAYDATT DAVE <jaydattdave10@gmail.com>
2025-11-18 17:57:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pr_7_mad/Person.kt
@@ -0,0 +1,31 @@
+package com.example.pr_7_mad
+
+import org.json.JSONObject
+import java.io.Serializable
+
+class Person(
+    var id: String,
+    var name: String,
+    var emailId: String,
+    var phoneNo: String,
+    var address: String,) : Serializable
+{
+    constructor(jsonObject: JSONObject) : this(
+        id = "",
+        name = "",
+        emailId = "",
+        phoneNo = "",
+        address = ""
+    ) {
+        id = jsonObject.getString("id")
+        emailId = jsonObject.getString("email")
+        phoneNo = jsonObject.getString("phone")
+        val profileJson = jsonObject.getJSONObject("profile")
+        name = profileJson.getString("name")
+        address = profileJson.getString("address")
+    }
+
+    override fun toString(): String {
+        return "$name\n$phoneNo\n$emailId\n$address"
+    }
+}
